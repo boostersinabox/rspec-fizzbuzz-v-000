150ac8406c1c2f7ffc60cbded70e12d203a95604
Stacy Carr <stacy@boostersinabox.com>
2018-05-04 21:05:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0
+    fizz_3 = fizzbuzz(3)
 print "Fizz"
 elsif int % 5 == 0
+fizz_5 = fizzbuzz(5)
 print "Buzz"
 elsif int % 15 == 0
+fizz_15 = fizzbuzz(15)
 print "FizzBuzz"
 else 
-  print nil
+  fizz_4 = fizzbuzz()
 end
